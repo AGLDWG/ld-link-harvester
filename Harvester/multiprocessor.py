dc5090d063400eacce2c51b42c154fd89816a984
Hashim-Jones <has112@csiro.au>
2019-03-19 15:44:44 +1000

    Started parallel scripts.

--- Harvester/multiprocessor.py
+++ Harvester/multiprocessor.py
@@ -7,15 +7,16 @@ __author__='Jake Hashim-Jones'
 
 import requests
 import signal
-import time
 import multiprocessing as mp
 from bs4 import BeautifulSoup
+from urllib.parse import urlparse
 from urllib.parse import urljoin
 from modules.lddatabase import LDHarvesterDatabaseConnector
 
 URI_in_file = 'single_URI.txt'
+URI_out_file = 'ld_seed_URIs.txt'
 database_file = 'ld-database.db'
-worker_count = 4
+workers = 4
 rdf_media_types = [
     "application/rdf+xml",
     "text/turtle",
@@ -59,19 +60,22 @@ def find_links_html(response_content, uri, seed, depth=0):
 
 
 def make_request(uri, depth, seed, header=global_header):
+    #print(len(visited))
     try:
         if depth <= recursion_depth_limit and uri not in visited:
-            print(uri)
             response = requests.get(uri, headers=header)
             visited[uri.strip('/#')] = True
         else:
             return False
     except:
+        print("Cannot Reach ", uri)
         dbconnector.insert_link(uri, crawlid, seed, failed=1)
         if uri == seed:
             dbconnector.insert_failed_seed(uri, crawlid, '000')
         dbconnector.commit()
-        return "Cannot Reach " + uri
+        return False
+    print(uri, depth)
+    print(response.headers['Content-type'])
     if response.status_code == 200:
         dbconnector.insert_link(uri, crawlid, seed)
         dbconnector.commit()
@@ -79,28 +83,49 @@ def make_request(uri, depth, seed, header=global_header):
         if uri.split('.')[-1] in rdf_file_ending:
             dbconnector.insert_valid_rdfuri(uri, crawlid, seed, file_format)
             dbconnector.commit()
-            return "LD!"
+            with open(URI_out_file, 'a') as dump:
+                dump.write(uri + '\n')
+            return True
         if file_format in rdf_media_types:
             dbconnector.insert_valid_rdfuri(uri, crawlid, seed, file_format)
             dbconnector.commit()
-            return "LD!"
+            with open(URI_out_file, 'a') as dump:
+                dump.write(uri + '\n')
+            return True
         elif file_format == 'text/html':
             try:
                 child_links = find_links_html(response.content, uri, seed, depth+1)
-                return child_links
             except Exception as er:
                 print(er, end='...')
-                return 'Cannot decode response. Continuing'
+                print('Cannot decode response from {}. Continuing'.format(uri))
+                return False
+            return child_links
         elif file_format in ['application/xhtml+xml']:
             pass
-        dbconnector.insert_link(uri, crawlid, seed)
-        return 'Unknown File Format - {}'.format(file_format)
     else:
         dbconnector.insert_link(uri, crawlid, seed, failed=1)
-        dbconnector.commit()
         if uri == seed:
-            dbconnector.insert_failed_seed(uri, crawlid, '000')
-        return 'Cannot retrieve document. Response {}'.format(response.status_code)
+            dbconnector.insert_failed_seed(uri, crawlid, response.status_code)
+        dbconnector.commit()
+        return False
+
+
+def setup_request(uri_triple):
+    uri, depth, seed = uri_triple
+    if depth == 0:
+        dbconnector.insert_crawl_seed(uri, crawlid)
+        dbconnector.commit()
+    response = make_request(uri, depth, seed)
+    return response
+
+def worker_function(w, uri_queue):
+    print("Started Worker {}.".format(w))
+    while not uri_queue.empty():
+        uri = uri_queue.get()
+        resp = setup_request(uri)
+        if isinstance(resp, list):
+            [uri_queue.put(child_uri) for child_uri in resp]
+    raise SystemExit(0)
 
 
 def close():
@@ -109,37 +134,10 @@ def close():
     dbconnector.close()
     print("Connection Closed.")
 
-start_sentinal = "start"
-end_sentinal = "end"
-def worker_pc(w, in_queue, out_queue):
-    #session = requests.Session()
-    out_queue.put(start_sentinal)
-    #print(in_queue.empty())
-    #print(in_queue.get())
-    while True:
-        if in_queue.empty():
-            break
-        uri, depth, seed = in_queue.get()
-        try:
-            if depth == 0:
-                dbconnector.insert_crawl_seed(uri, crawlid)
-                dbconnector.commit()
-            print(uri)
-            response = make_request(uri, depth, seed, global_header)
-        except Exception as e:
-            response = None
-            out_queue.put((uri, e))
-        if isinstance(response, list):
-            [in_queue.put(uri) for uri in response]
-        if response:
-            out_queue.put((uri, response))
-    print("Worker {} done.".format(w))
-    out_queue.put(end_sentinal)
-    raise SystemExit(0)
-
 
 if __name__ == '__main__':
     visited = {}
+    open(URI_out_file, 'w').close()
     dbconnector = LDHarvesterDatabaseConnector(database_file)
     crawlid = dbconnector.get_new_crawlid()
     dbconnector.insert_crawl(crawlid)
@@ -147,40 +145,19 @@ if __name__ == '__main__':
     signal.signal(signal.SIGINT, close)
     initial_URI_list = [(URI.strip(), 0, URI.strip()) for URI in open(URI_in_file, 'r')]
 
+    uri_queue = mp.Queue()
+    [uri_queue.put(uri) for uri in initial_URI_list]
+    worker_array = []
+    for i in range(workers):
+        worker = mp.Process(target=worker_function, args=(i, uri_queue))
+        worker_array.append(worker)
+    [worker.start() for worker in worker_array]
+
+    exit()
     try:
-        work_queue = mp.Queue()
-        [work_queue.put(uri) for uri in initial_URI_list]
-        resp_queue = mp.Queue()
-        workers = []
-        for i in range(worker_count):
-            worker = mp.Process(target=worker_pc, args=(i+1, work_queue, resp_queue))
-            workers.append(worker)
-        begin = time.time()
-        [worker.start() for worker in workers]
-        time.sleep(0.1)
-        threads_started = 0
-        threads_ended = 0
-        while True:
-            resp_tuple = resp_queue.get()
-            if resp_tuple == start_sentinal:
-                threads_started += 1
-                continue
-            elif resp_tuple == end_sentinal:
-                threads_ended += 1
-                if threads_ended == worker_count:
-                    break
-                else:
-                    continue
-            #if isinstance(resp_tuple, Exception):
-            #    print("{} : {}".format(str(resp_tuple[0]), str(resp_tuple[1])))
-            #else:
-            #    print("{} : {}".format(str(resp_tuple[0]), str(resp_tuple[1].status_code)))
-        [worker.join() for worker in workers]
-        end = time.time()
-        print("Duration: {} seconds".format(end - begin))
+        bulk_request(initial_URI_list)
     except Exception as er:
         print(er)
         close()
         exit(1)
     close()
-
