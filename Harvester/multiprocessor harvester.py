353ec42340780204adb067ae03b09ec7b2f8dd46
Hashim-Jones <has112@csiro.au>
2019-03-21 15:20:06 +1000

    Complete database integration of parallel crawler.

--- Harvester/multiprocessor harvester.py
+++ Harvester/multiprocessor harvester.py
@@ -1,12 +1,14 @@
 import requests
 from multiprocessing import Queue, Process, Manager
 import time
+import signal
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 from modules.lddatabase import LDHarvesterDatabaseConnector
 
 URL_BATCH = [(url.strip(), 0, url.strip()) for url in open('single_URI.txt')]
 DATABASE_FILE = 'ld-database.db'
+RECURSION_DEPTH_LIMIT = 4
 PROC_COUNT = 8
 RDF_MEDIA_TYPES = [
     "application/rdf+xml",
@@ -29,7 +31,15 @@ GLOBAL_HEADER = {
     'Accept': ",".join(RDF_FORMATS),
     'User-Agent': 'LD Link Harvester'
 }
-#print(URL_BATCH)
+
+
+def close():
+    dbconnector.end_crawl(crawlid)
+    dbconnector.commit()
+    dbconnector.close()
+    print("Connection Closed.")
+
+
 def find_links_html(response_content, uri, seed, depth=0):
     # Function to parse links from a web document (presumably html)
     links = []
@@ -46,6 +56,7 @@ def find_links_html(response_content, uri, seed, depth=0):
             links.append((link, depth, seed))
     #print(links)
     return links
+
 
 def process_response(response, uri, seed, depth):
     if response.status_code == 200:
@@ -87,29 +98,24 @@ def process_response(response, uri, seed, depth):
             return enhanced_resp
     else:
         #dbconnector.insert_link(uri, crawlid, seed, failed=1)
-        if uri == seed: #Move this somewhere else??
-            enhanced_resp = {'url': uri,
-                             'opcode': 1,
-                             'params': {'source': seed, 'failed': 1}}
-            #dbconnector.insert_failed_seed(uri, crawlid, response.status_code)
-            #dbconnector.commit()
-            return enhanced_resp
-        else:
-            enhanced_resp = {'url': uri,
-                             'opcode': 2,
-                             'params': {'source': seed, 'failed': 1}}
-            return enhanced_resp
+        enhanced_resp = {'url': uri,
+                         'opcode': 2,
+                         'params': {'source': seed, 'failed': 1}}
+        return enhanced_resp
+
 
 start_sentinal = "start"
 end_sentinal = "end"
 def worker_fn(p, in_queue, out_queue, visited):
     print("Process {} started.".format(p))
+    signal.signal(signal.SIGTERM, close)
+    signal.signal(signal.SIGINT, close)
     out_queue.put(start_sentinal)
     while not in_queue.empty():
         url = in_queue.get()
         url, depth, seed = url
         try:
-            if url not in visited:
+            if url not in visited and depth <= RECURSION_DEPTH_LIMIT:
                 visited[url.strip('/#')] = True
                 resp = requests.get(url, headers=GLOBAL_HEADER)
             else:
@@ -145,6 +151,8 @@ if __name__ == "__main__":
     dbconnector = LDHarvesterDatabaseConnector(DATABASE_FILE)
     crawlid = dbconnector.get_new_crawlid()
     dbconnector.insert_crawl(crawlid)
+    signal.signal(signal.SIGTERM, close)
+    signal.signal(signal.SIGINT, close)
 
     manager = Manager()
     visited = manager.dict()
@@ -161,7 +169,12 @@ if __name__ == "__main__":
     time.sleep(0.1)
     threads_started = 0
     threads_ended = 0
+    i = 0
     while True:
+        if i >= 50:
+            dbconnector.commit()
+            i =- 1
+        i += 1
         resp_tuple = resp_queue.get()
         if resp_tuple == start_sentinal:
             threads_started += 1
@@ -172,11 +185,29 @@ if __name__ == "__main__":
                 break
             else:
                 continue
+        if isinstance(resp_tuple[0], dict):
+            '''
+            OPCODES: 
+            0 = Insert Seed (Deprecated)
+            1 = Insert Failed Seed (Handled by 2)
+            2 = Insert Link (Failed or otherwise)
+            3 = Insert RDF Data
+            '''
+            opcode = resp_tuple[0]['opcode']
+            if resp_tuple[0]['url'] == resp_tuple[0]['params']['source']:
+                dbconnector.insert_crawl_seed(uri=resp_tuple[0]['url'], crawlid=crawlid)
+            if opcode == 2:
+                dbconnector.insert_link(uri=resp_tuple[0]['url'], crawlid=crawlid, source=resp_tuple[0]['params']['source'], failed=resp_tuple[0]['params']['failed'])
+                if resp_tuple[0]['params']['failed'] == 1 and resp_tuple[0]['url'] == resp_tuple[0]['params']['source']:
+                    dbconnector.insert_failed_seed(uri=resp_tuple[0]['url'], crawlid=crawlid, code=resp_tuple[1].status_code)
+            if opcode == 3:
+                dbconnector.insert_valid_rdfuri(uri=resp_tuple[0]['url'], crawlid=crawlid, source=resp_tuple[0]['params']['source'], response_format=resp_tuple[0]['params']['format'])
         if isinstance(resp_tuple[1], Exception):
             print("{} : {}".format(str(resp_tuple[0]['url']), str(resp_tuple[1])))
         else:
             print("{} : {}".format(str(resp_tuple[0]['url']), str(resp_tuple[1].status_code)))
     [p.join() for p in worker_procs]
     end = time.time()
+    close()
     print(visited)
     print("Duration: {} seconds".format(end - begin))
