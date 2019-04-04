5b86dbff24973d47895c54f528860a587c68d9b9
Hashim-Jones <has112@csiro.au>
2019-04-04 14:10:46 +1000

    Revert "Added additional database queries for analysing crawl performance."
    This reverts commit fde907fd1702ea20eeacfd262c2826aed23a5fa7.

--- database/queries.sql
+++ database/queries.sql
@@ -1,4 +1,3 @@
---QUERIES RELATED TO VALIDITY OF SEEDS
 -- Retrieve a list of URIs that need to be reattempted.
 SELECT seedURI, COUNT(seedURI)
 FROM FailedSeed
@@ -20,71 +19,6 @@ WHERE seedURI in (
     GROUP BY seedURI
     HAVING COUNT(seedURI) >= 2);
 
--- Retrieve seeds that have been invalidated
-SELECT *
-FROM SEED
-WHERE invalidationDate IS NOT NULL;
-
--- Retrieve a list of seeds that are still valid
-SELECT *
-FROM SEED
-WHERE invalidationDate IS NULL;
-
-
--- QUERIES RELATED TO THE RDF LINKS
 -- Find a list of URIs Pointing to Linked Data
 SELECT rdfSeedURI FROM RdfURI;
 
--- List the formats (and counts) that the Linked data was found in
-SELECT contentFormat, COUNT(contentFormat)
-FROM RdfURI
-GROUP BY contentFormat;
-
--- List the formats (and counts) that the Linked data was found in during a specific crawl.
-SELECT contentFormat, COUNT(contentFormat)
-FROM RdfURI
-WHERE crawlId=1 -- CrawlId goes here
-GROUP BY contentFormat;
-
--- List the number of URIs found per each seed
-SELECT originSeedURI, COUNT(DISTINCT rdfSeedURI)
-FROM RdfURI
-GROUP BY originSeedURI;
-
-
---QUERIES RELATING TO  CRAWLS
---List the number of URLs Tested Per Crawl
-SELECT crawlId, COUNT(address)
-FROM Link
-GROUP BY crawlId;
-
---
-SELECT
-    Link.crawlId AS Crawl,
-    Failed.failCount + Successful.successCount AS 'Total Requests',
-    Failed.failCount AS 'Failed Requests',
-    Successful.successCount AS 'Successful Requests',
-    LinkedData.ldSeeds AS 'Linked Data Seeds'
-FROM
-    Link,
-    (SELECT crawlId, count(crawlId) as failCount
-        FROM Link
-        WHERE failed=1
-        GROUP BY crawlId) AS Failed,
-    (SELECT crawlId, count(crawlId) as successCount
-        FROM Link
-        WHERE failed=0
-        GROUP BY crawlId) AS Successful,
-    (SELECT crawlId, COUNT(rdfSeedURI) as ldSeeds
-        FROM RdfURI
-        GROUP BY crawlId) AS LinkedData
-WHERE
-    Link.crawlId = Failed.crawlId AND
-    Link.crawlId = Successful.crawlId AND
-    Link.crawlId = LinkedData.crawlId
-GROUP BY Link.crawlId;
-
---Get the count of each format encountered during a crawl (can filter for specific crawl if necessary)
-SELECT crawlId, contentFormat, COUNT(contentFormat)
-FROM Link
-GROUP BY crawlId, contentFormat;
