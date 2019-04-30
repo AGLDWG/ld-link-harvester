4f0c745d3de926d1d41ccc3cf89c5b9923344075
Jake Hashim-Jones <has112@lw-78-cdc.it.csiro.au>
2019-04-30 13:25:45 +1000

    Modified low_resource_run.sh to improve function.

--- harvester/low_resource_run.sh
+++ harvester/low_resource_run.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 prefix="aus-domain-urls_"
-script_command="python3 dummy_run.py"
+script_command="python3 __init__.py"
 
 mkdir -p ./data/completed
 
-for line in $(ls data);
+for path in data/*;
 do
+	line=`basename $path`
+	echo $line
 	if [[ $line == $prefix* ]]; then
 		echo "Starting seeds in $line"
-		output=$($script_command $line 2>&1 >/dev/null)
+		output=$($script_command data/$line 2>&1 >/dev/null)
 		status=$?
 		if [ $status != 0 ]; then
 		    	echo "Something went wrong in $line. Error code: $status"
